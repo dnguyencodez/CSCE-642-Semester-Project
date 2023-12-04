a363bf1de31789295e456e90b4a235baf9f6adcd
Nick <nick.robert202@gmail.com>
2023-12-03 23:06:30 -0600

    dqn working, tuning now

--- lane_keeping_dqn.py
+++ lane_keeping_dqn.py
@@ -11,6 +11,10 @@ from vista.entities.agents.Dynamics import tireangle2curvature
 from vista.utils import logging, misc
 import random
 import cv2
+import matplotlib.pyplot as plt
+
+from warnings import filterwarnings
+filterwarnings(action='ignore', category=DeprecationWarning, message='In future, it will be an error for \'np.bool_\' scalars to be interpreted as an index')
 
 NUM_ACTIONS = 6191
 
@@ -30,7 +34,7 @@ class DQN(nn.Module):
 
         # Flatten the output of the final convolutional layer
         self.flatten_size = self._get_conv_output((3, 200, 320))
-        print(f"flatten_size = {self.flatten_size}")
+        # print(f"flatten_size = {self.flatten_size}")
         # Fully connected layers
         self.fc1 = nn.Linear(self.flatten_size, 512)
         self.fc2 = nn.Linear(512, action_dim)
@@ -45,12 +49,12 @@ class DQN(nn.Module):
             output = F.relu(self.pool2(self.conv2(output)))
             output = F.relu(self.pool3(self.conv3(output)))
 
-            print(f"output.shape = {output.shape}")
+            # print(f"output.shape = {output.shape}")
             return int(np.prod(output.size()))
 
     def forward(self, state):
-        print("__FUNCTION__forward")
-        print(f"\tstate.shape = {state.shape}")
+        # print("__FUNCTION__forward")
+        # print(f"\tstate.shape = {state.shape}")
         # Convert state to float and scale if necessary
         state = state.float() / 255.0  # Scale images to [0, 1]
 
@@ -59,12 +63,12 @@ class DQN(nn.Module):
         x = F.relu(self.pool3(self.conv3(x)))
 
         # Flatten and pass through fully connected layer
-        print(f"\tx_reshaped = {x.shape}")
+        # print(f"\tx_reshaped = {x.shape}")
         x = x.reshape(x.size(0), -1)
-        print(f"\tx_reshaped = {x.shape}")
+        # print(f"\tx_reshaped = {x.shape}")
         x = F.relu(self.fc1(x))
         q_values = F.relu(self.fc2(x))
-        print(f"\tq_values.shape = {q_values.shape}")
+        # print(f"\tq_values.shape = {q_values.shape}")
         return q_values
 
 """
@@ -98,7 +102,7 @@ class environment:
         self.prev_xy = np.zeros((2, ))
 
         self.action_space = self.empty_action_space()
-        print(f"self.action_space = {self.action_space}")
+        # print(f"self.action_space = {self.action_space}")
 
     def empty_action_space(self):
         # creating an action space where curvature ranges from -0.2 to 0.2 and speed ranges from 0 to 15
@@ -129,10 +133,12 @@ class environment:
         road_half_width = self.agent.trace.road_width / 2.
         out_of_lane = np.abs(self.agent.relative_state.x) > road_half_width
 
-        maximal_rotation = np.pi / 10
+        not_near_center = np.abs(self.agent.relative_state.x) > road_half_width / 4
+
+        maximal_rotation = np.pi / 2
         exceed_max_rotation = np.abs(self.agent.steering) > maximal_rotation
 
-        done = self.agent.done or out_of_lane or exceed_max_rotation
+        done = self.agent.done or out_of_lane# or exceed_max_rotation
 
         # get other info
         info = misc.fetch_agent_info(self.agent)
@@ -145,27 +151,30 @@ class environment:
         self.distance += dd
         self.prev_xy = current_xy
         info['distance'] = self.distance
-
         # Compute reward
-        # reward = -1 if done else 0
-        reward = 0
-        if out_of_lane and exceed_max_rotation:
-            reward = -5
-        elif out_of_lane or exceed_max_rotation:
-            reward = -2.5
+        reward = 0 if not self.agent.done else 300
+        if out_of_lane:
+            reward = -100
+        elif exceed_max_rotation:
+            reward = -0.5
         else:
-            reward = dd * 10
+            reward = dd * 50
+        
+        if not_near_center:
+            reward -= 0.5
+        else:
+            reward += 2
 
         return next_state, reward, done, info
     
 
     def epsilon_greedy_action(self, state, epsilon):
         # Restructuring the states to match the input of the conv layers
-        print("__FUNCTION__, epsilon_greedy_action")
+        # print("__FUNCTION__, epsilon_greedy_action")
         # print(f"\tstate = {state}")
-        print(f"\tstate.shape = {state.shape}")
+        # print(f"\tstate.shape = {state.shape}")
         state = state.permute(0, 3, 1, 2)
-        print(f"\tstate.shape = {state.shape}")
+        # print(f"\tstate.shape = {state.shape}")
         prob = np.random.uniform()
 
         if prob < epsilon:
@@ -196,7 +205,7 @@ class ReplayBuffer:
 Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state', 'done'))
 
 def optimize_model(memory, batch_size, gamma):
-    print("__FUNCTION__optimize_model()")
+    # print("__FUNCTION__optimize_model()")
     if memory.size() < batch_size:
         return
     
@@ -205,51 +214,55 @@ def optimize_model(memory, batch_size, gamma):
 
     # convert to tensors and move to device
     state_batch = torch.cat([s for s in batch.state]).to(device)
-    action_batch = torch.cat([a for a in batch.action]).to(device)
-    # action_batch = torch.cat([torch.tensor([a]).to(device) for a in batch.action])
+    # action_batch = torch.cat([a for a in batch.action]).to(device)
+    action_batch = torch.cat([torch.tensor([a]).to(device) for a in batch.action])
     reward_batch = torch.cat([torch.tensor([r]).to(device) for r in batch.reward])
     next_state_batch = torch.cat([s for s in batch.next_state if s is not None]).to(device)
     done_batch = torch.cat([torch.tensor([d]).to(device) for d in batch.done])
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool).to(device)
+    # non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool).to(device)
     
+    # print(f"\tstate_batch.shape = {state_batch.shape}")
     state_batch = state_batch.permute(0, 3, 1, 2)
-    print(f"\tstate_batch.shape = {state_batch.shape}")
-    print(f"\taction_batch.shape = {action_batch.shape}")
-    print(f"\treward_batch.shape = {reward_batch.shape}")
-    print(f"\tnext_state_batch.shape = {next_state_batch.shape}")
-    print(f"\tnon_final_mask.shape = {non_final_mask.shape}")
-    print(f"\tdone_batch.shape = {done_batch.shape}")
-    print(f"\tdone_batch = {done_batch}")
-    print(f"\tnon_final_mask = {non_final_mask}")
+    next_state_batch = next_state_batch.permute(0, 3, 1, 2)
+    # print(f"\tstate_batch.shape = {state_batch.shape}")
+    # print(f"\taction_batch.shape = {action_batch.shape}")
+    # print(f"\treward_batch.shape = {reward_batch.shape}")
+    # print(f"\tnext_state_batch.shape = {next_state_batch.shape}")
+    # print(f"\tdone_batch.shape = {done_batch.shape}")
     # Compute Q
     current_q = network(state_batch)
-    print(f"\tcurrent_q.shape = {current_q.shape}")
-    print(f"\taction_batch.unsqueeze(1).shape = {action_batch.unsqueeze(1).shape}")
-    print(f"\taction_batch.unsqueeze(1).long().shape = {action_batch.unsqueeze(1).long().shape}")
-    current_q = torch.gather(current_q, dim=1, index=action_batch).squeeze(-1)
-    print(f"\tcurrent_q.shape = {current_q.shape}")
-    print(f"\tcurrent_q = {current_q}")
+    # print("  __FUNCTION__optimize_model()")
+    # print(f"\tcurrent_q.shape = {current_q.shape}")
+    # print(f"\taction_batch.unsqueeze(1).shape = {action_batch.unsqueeze(1).shape}")
+    # print(f"\taction_batch.unsqueeze(1).long().shape = {action_batch.unsqueeze(1).long().shape}")
+    current_q = torch.gather(current_q, dim=1, index=action_batch.unsqueeze(1).long()).squeeze(-1)
+    # print(f"\tcurrent_q.shape = {current_q.shape}")
+    # print(f"\tcurrent_q = {current_q}")
 
-    print(f"\tlen(next_state_batch) = {len(next_state_batch)}")
+    # print(f"\tlen(next_state_batch) = {len(next_state_batch)}")
 
     with torch.no_grad():
         # compute target Q
         target_q = torch.full([len(next_state_batch)], 0, dtype=torch.float32)
+        # target_q = torch.zeros(batch_size, NUM_ACTIONS, dtype=torch.float32)
+
         for idx in range(len(next_state_batch)):
             reward = reward_batch[idx]
-            next_state = next_state_batch[idx]
+            next_state = next_state_batch[idx].unsqueeze(0)
             done = done_batch[idx]
             if (done):
                 target_q[idx] = reward
+                # print(f"\t\t\treward = {reward}")
             else:
+                # print(f"\tstate_batch.shape = {state_batch.shape}")
+                # print(f"\t\t\tnext_state.shape = {next_state.shape}")
                 q_values = target_network(next_state)
-                print(f"\t\tt_q_values.shape = {q_values.shape}")
-                max_q = q_values[torch.argmax(q_values)]
+                # print(f"\t\t\tt_q_values.shape = {q_values.shape}")
+                max_q = q_values[0][torch.argmax(q_values)]
                 target = reward + gamma * max_q
                 target_q[idx] = target
 
-    print(f"\ttarget_q.shape = {target_q.shape}")
-    
+    # print(f"\ttarget_q.shape = {target_q.shape}")
 
     # Compute Huber loss
     loss_q = loss_fn(current_q, target_q)
@@ -268,9 +281,24 @@ if __name__ == '__main__':
     parser.add_argument('--trace-path',
                         type=str,
                         nargs='+',
-                        help='Path to the traces to use for simulation')
+                        help='Path to the traces to use for simulation',
+                        required=True)
+    parser.add_argument('--version',
+                        type=str,
+                        nargs='+',
+                        help='version number for model naming',
+                        required=False)
+    parser.add_argument('--operation',
+                        type=str,
+                        nargs='+',
+                        help='Load or New',
+                        required=True)
+    parser.add_argument('--save-path',
+                        type=str,
+                        nargs='+',
+                        help='Path to the saved model state',
+                        required=False)
     args = parser.parse_args()
-
     trace_config={'road_width': 4}
     car_config={
             'length': 5.,
@@ -286,29 +314,124 @@ if __name__ == '__main__':
     env = environment(args.trace_path, trace_config, car_config, sensor_config)
     display = vista.Display(env.world)
 
-    """
-    Initializing hyper-parameters and beginning the training loop
-    """
-    replay_buffer = ReplayBuffer(10000)
-    batch_size = 4
-    gamma = 0.99 
-    epsilon_start = 1.0
-    epsilon_end = 0.01
-    epsilon_decay = 0.995
-    num_episodes = 500
-    target_update = 10  # Update target network every 10 episodes
+    if args.operation[0].lower() == 'new':
 
-    epsilon = epsilon_start
-    for episode in range(num_episodes):
+        """
+        Initializing hyper-parameters and beginning the training loop
+        """
+        replay_buffer = ReplayBuffer(10000)
+        batch_size = 32
+        gamma = 0.99 
+        epsilon_start = 1.0
+        epsilon_end = 0.01
+        epsilon_decay = 0.995
+        num_episodes = 200
+        target_update = 10  # Update target network every 10 episodes
+        max_num_steps = 10000
+
+        best_dict = {}
+        best_dict_reward = -1e10
+
+        # per episode
+        rewards = np.array([])
+        num_steps = np.array([])
+
+        epsilon = epsilon_start
+        for episode in range(num_episodes):
+            state = env.reset()['camera_front']
+            # print(f"main, state.shape after reset = {state.shape}")
+            # print(state)
+            display.reset()
+            total_reward = 0
+            done = False
+            step = 0
+
+            while step < max_num_steps and not done:
+                # Convert state to the appropriate format and move to device
+                state_tensor = torch.from_numpy(state).unsqueeze(0).to(device)
+
+                # Select action using epsilon greedy policy
+                action = env.epsilon_greedy_action(state_tensor, epsilon)
+                next_state, reward, done, _ = env.step(action)
+                next_state = next_state['camera_front']
+
+                # Convert next_state to tensor and move to device
+                next_state_tensor = torch.from_numpy(next_state).unsqueeze(0).to(device) if next_state is not None else None
+
+                # Store the transition in the replay buffer
+                # action_tensor = torch.zeros(1, NUM_ACTIONS, dtype=torch.int64)
+                # print(f"action_tensor = {action_tensor}")
+                # print(f"action_tensor.shape = {action_tensor.shape}")
+                # print(f"action = {action}")
+                # print(f"action.shape = {action.shape}")
+                # print(f"env.action_idx = {env.action_idx}")
+                # action_tensor[0][env.action_idx] = 1
+
+                replay_buffer.store((state_tensor, env.action_idx, reward, next_state_tensor, done))
+
+                state = next_state
+                total_reward += reward
+
+                vis_img = display.render()
+
+                # Optimize the model if the replay buffer has enough samples
+                optimize_model(replay_buffer, batch_size, gamma)
+
+                if step % target_update == 0 or done:
+                    target_network.load_state_dict(network.state_dict())
+
+                step += 1
+                # cv2.imshow(f'Car Agent in Episode {episode}', vis_img[:, :, ::-1])
+                # cv2.waitKey(5)
+            rewards = np.append(rewards, total_reward / step)
+            num_steps = np.append(num_steps, step)
+
+            if total_reward > best_dict_reward:
+                best_dict = target_network.state_dict()
+                best_dict_reward = total_reward
+
+            print(f'Episode {episode}: Total Reward: {total_reward}, Epsilon: {epsilon}, NumSteps: {step}')
+
+            # Update epsilon
+            epsilon = max(epsilon_end, epsilon_decay * epsilon)
+        
+        # Save the model's state dictionary
+        torch.save(target_network.state_dict(), 'saves/v'+args.version[0]+'_final_dqn_network_nn_model.pth')
+        torch.save(best_dict, 'saves/v'+args.version[0]+'_best_dqn_network_nn_model.pth')
+
+        eps = np.arange(0, num_episodes)
+        print(f"rewards = {rewards}")
+        print(f"num_steps = {num_steps}")
+
+        # Create a line graph
+        plt.plot(eps, rewards)
+
+        # Add labels and a title
+        plt.xlabel('Training Episodes')
+        plt.ylabel('Average Reward per Episode')
+        plt.title('Average Reward')
+
+        # Display the plot
+        plt.show()
+
+        plt.plot(eps, num_steps)
+        plt.xlabel('Training Episodes')
+        plt.ylabel('Number of Steps per Episode')
+        plt.title('Steps per Episode')
+        # Display the plot
+        plt.show()
+
+
+    elif args.operation[0].lower() == 'load':
+        target_network.load_state_dict(torch.load(args.save_path[0]))
+        target_network.eval()
+
         state = env.reset()['camera_front']
-        print(f"main, state.shape after reset = {state.shape}")
-        # print(state)
         display.reset()
         total_reward = 0
         done = False
         step = 0
-
-        while not done:
+        while step < max_num_steps and not done:
             # Convert state to the appropriate format and move to device
             state_tensor = torch.from_numpy(state).unsqueeze(0).to(device)
 
@@ -321,15 +444,15 @@ if __name__ == '__main__':
             next_state_tensor = torch.from_numpy(next_state).unsqueeze(0).to(device) if next_state is not None else None
 
             # Store the transition in the replay buffer
-            action_tensor = torch.zeros(1, NUM_ACTIONS, dtype=torch.int64)
-            print(f"action_tensor = {action_tensor}")
-            print(f"action_tensor.shape = {action_tensor.shape}")
-            print(f"action = {action}")
-            print(f"action.shape = {action.shape}")
-            print(f"env.action_idx = {env.action_idx}")
+            # action_tensor = torch.zeros(1, NUM_ACTIONS, dtype=torch.int64)
+            # print(f"action_tensor = {action_tensor}")
+            # print(f"action_tensor.shape = {action_tensor.shape}")
+            # print(f"action = {action}")
+            # print(f"action.shape = {action.shape}")
+            # print(f"env.action_idx = {env.action_idx}")
+            # action_tensor[0][env.action_idx] = 1
 
-            action_tensor[0][env.action_idx] = 1
-            replay_buffer.store((state_tensor, action_tensor, reward, next_state_tensor, done))
+            # replay_buffer.store((state_tensor, env.action_idx, reward, next_state_tensor, done))
 
             state = next_state
             total_reward += reward
@@ -337,25 +460,10 @@ if __name__ == '__main__':
             vis_img = display.render()
 
             # Optimize the model if the replay buffer has enough samples
-            optimize_model(replay_buffer, batch_size, gamma)
-
-            if step % target_update == 0 or done:
-                target_network.load_state_dict(network.state_dict())
+            # optimize_model(replay_buffer, batch_size, gamma)
 
             step += 1
-            # cv2.imshow(f'Car Agent in Episode {episode}', vis_img[:, :, ::-1])
-            # cv2.waitKey(20)
-
-        print(f'Episode {episode}: Total Reward: {total_reward}, Epsilon: {epsilon}')
-
-        # Update epsilon
-        epsilon = max(epsilon_end, epsilon_decay * epsilon)
-
-        # Update the target network
-        # if episode % target_update == 0:
-            # target_nn.load_state_dict(behavior_nn.state_dict())
-    
-    # Save the model's state dictionary
-    torch.save(network.state_dict(), 'dqn_network_nn_model.pth')
-
-
+            cv2.imshow(f'Car Agent in Episode {episode}', vis_img[:, :, ::-1])
+            cv2.waitKey(5)
+    else:   
+        print("incorrect operation command, only 'new' or 'load' allowed")
